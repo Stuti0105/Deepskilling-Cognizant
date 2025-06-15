4ada7bb7158a607a6949c1ff2fcd273d1a3371f6
Stuti0105 <147245679+Stuti0105@users.noreply.github.com>
2025-06-15 19:10:37 +0530

    Add files via upload

---
+++ Logger.java
@@ -0,0 +1,27 @@
+package Cognizant;
+
+public class Logger {
+		// TODO Auto-generated method stub
+		private static Logger instance;
+		
+		private Logger() {
+			System.out.println("Logger instance created.");
+		}
+		
+		public static Logger getInstance() {
+			if(instance==null) {
+				instance=new Logger();
+			}
+			return instance;
+			
+		}
+		
+		
+		public void log(String message) {
+			System.out.println("Log:" +message);
+		}
+		
+
+	}
+
+
